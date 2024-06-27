72660951b98d617e1afeb8503d7243250b174fe1
Daniel Trejo <dwtrejo14@comcast.net>
2024-06-27 16:24:48 -0400

    Complete Lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+
+    fibonacci = [0, 1]  # Initialize the Fibonacci sequence with the first two numbers
+    for i in range(2, length):
+        fibonacci.append(fibonacci[i - 1] + fibonacci[i - 2])  # Calculate the next Fibonacci number
+    print(fibonacci) 
